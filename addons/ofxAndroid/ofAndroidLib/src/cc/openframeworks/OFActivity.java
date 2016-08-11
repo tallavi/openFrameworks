3d03f11f407ccad9179530d9708c4590e9a24255
Artem Pyatishev <Artemp@trailze.com>
2016-08-11 15:11:07 +0300

    Android - add warnings to init view if main_layout not found or not found  of_gl_surface_container.

--- addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFActivity.java
+++ addons/ofxAndroid/ofAndroidLib/src/cc/openframeworks/OFActivity.java
@@ -22,18 +22,22 @@ public abstract class OFActivity extends Activity{
         	Log.v("OF","trying to find class: "+packageName+".R$layout");
 			Class<?> layout = Class.forName(packageName+".R$layout");
 			View view = this.getLayoutInflater().inflate(layout.getField("main_layout").getInt(null),null);
-			if(view == null)
+			if(view == null) {
+				Log.w("OF", "Could not find main_layout.xml.");
 				throw new Exception();
+			}
 			this.setContentView(view);
 			
 			Class<?> id = Class.forName(packageName+".R$id");
 			mOFGlSurfaceContainer = (ViewGroup)this.findViewById(id.getField("of_gl_surface_container").getInt(null));
 			
-			if(mOFGlSurfaceContainer == null)
+			if(mOFGlSurfaceContainer == null) {
+				Log.w("OF", "Could not find of_gl_surface_container in main_layout.xml. Copy main_layout.xml from latest empty example to fix this warning.");
 				throw new Exception();
+			}
 			
 		} catch (Exception e) {
-			Log.e("OF", "couldn't create view from layout falling back to GL only",e);
+			Log.w("OF", "couldn't create view from layout falling back to GL only",e);
 			mOFGlSurfaceContainer = new FrameLayout(this);
 	        this.setContentView(mOFGlSurfaceContainer);
 		}
